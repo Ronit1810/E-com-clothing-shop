0bf739d4958dcb0d5e186b3f56f6f53087ae72f6
RONIT B PATEL <ronitpatel1810@gmail.com>
2023-10-27 22:49:23 +0530

    initial and final commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+var mainimage = document.getElementById("main-image");
+var smallimage = document.getElementsByClassName("small-img");
+
+smallimage[0].onclick = function(){
+    mainimage.src = smallimage[0].src;
+}
+smallimage[1].onclick = function(){
+    mainimage.src = smallimage[1].src;
+}
+smallimage[2].onclick = function(){
+    mainimage.src = smallimage[02].src;
+}
+smallimage[3].onclick = function(){
+    mainimage.src = smallimage[3].src;
+}
+
